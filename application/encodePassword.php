08a7dcd9ffbf964bd8d23535ab6c4c43c6052716
Renato R. R. de Oliveira <renatorro@comp.ufla.br>
2016-08-15 16:58:06 -0300

    Criando branch para o site.|

--- application/encodePassword.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$pass = "";
-	if (isset($_POST['tst_password']))
-		$pass = sha1($_POST['tst_password']);
-	else
-		die("<b>Forbidden.</b>");
-	header("Location: testTrials.php?tst_id=".$_POST['tst_id']."&tst_password=".$pass);
-?>
--- application/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-	@session_start();
-	unset($_SESSION['logged']);
-	unset($_SESSION['user']);
-	@session_write_close();
-	header("Location: ./");
-?>
